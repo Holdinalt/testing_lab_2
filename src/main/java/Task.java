ff0ab4bd4043a4f9a1f34cf7f3065aed161c39d3
Holdest White <zbsholdest@mail.ru>
2023-03-02 14:42:31 +0300

    added task

---
+++ src/main/java/Task.java
@@ -0,0 +1,43 @@
+import Logarithmic.Ln;
+import Logarithmic.Log;
+import Trigonometric.*;
+
+public class Task {
+
+    private final Sin sin;
+    private final Cos cos;
+    private final Tg tg;
+    private final Ctg ctg;
+    private final Sec sec;
+    private final Csc csc;
+    private final Log log;
+
+    public Task() {
+        this.sin = new Sin();
+        this.cos = new Cos(sin);
+        this.tg = new Tg(sin, cos);
+        this.ctg = new Ctg(sin, cos);
+        this.sec = new Sec(cos);
+        this.csc = new Csc(sin);
+        this.log = new Log(new Ln());
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private double higherThanZero(double x){
+        double firstBlank = Math.pow((tg.execute(x) + sin.execute(x)) - ctg.execute(x), 3);
+        double secondBlank = (csc.execute(x) / sin.execute(x)) - sec.execute(x);
+        double thirdBlank = (Math.pow(sec.execute(x), 2)) - sin.execute(x);
+
+        return firstBlank / secondBlank / thirdBlank;
+    }
+
+    private double lessThanZero(double x){
+        double temp =  ((((log.execute(x, 5) - log.execute(x, 10)) + log.execute(x, 5)) - log.execute(x, 10)) -
+                - (log.execute(x, 5) * log.execute(x, 3)));
+
+        return Math.pow(temp, 3);
+    }
+}
