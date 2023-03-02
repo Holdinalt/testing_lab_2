e6f42c54a742d520d1e83f62c214b1a3c7e80bb8
tchn11 <akonany11@gmail.com>
2023-03-02 20:10:13 +0300

    Implement LN function

--- src/main/java/Mathematics/Logarithmic/Ln.java
+++ src/main/java/Mathematics/Logarithmic/Ln.java
@@ -10,7 +10,28 @@ public class Ln extends Writable implements LogExecutable{
             throw new Exception("Class Ln can count only E base");
         }
 
-        double out = 1; // Сделать out человеком
+
+        double summ = 0;
+        if (digit >= 0 && digit <= 2)
+        {
+            digit -= 1;
+
+            for (int i = 1; i < 5000; i += 2)
+            {
+                summ += (Math.pow(digit, i)/(double)i - Math.pow(digit, i+1)/(double)(i+1));
+            }
+        }
+        else if (digit > 2)
+        {
+            digit = (digit / (digit - 1));
+
+            for ( int i = 1; i < 1000; i ++ )
+            {
+                summ += 1.0 / (i * Math.pow(digit, i));
+            }
+        }
+
+        double out = summ; // Сделать out человеком
 
         tryWriteToFile(new String[] {String.valueOf(digit), String.valueOf(out), this.getClass().getName()});
 
